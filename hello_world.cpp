5d5437c3834dc59c71b0f02f15d953836372626c
kodxxl <xsrc@mail.ru>
2022-12-04 19:56:09 +0400

    New realy neccesery function

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,9 +5,16 @@ void style()
   std::cout << "///--------------///" << std::endl;
 }
 
+int return_zero()
+{
+  return 0;
+}
+
 int main()
 {
   style();
   std::cout << "Hello, world" << std::endl;
   style();
+
+  return_zero();
 }
